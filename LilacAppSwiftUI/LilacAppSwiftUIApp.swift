a272a4687e8b3a8e0a4edd52b806368d8fcab08f
NijatNaghiyev <nicat614@icloud.com>
2025-04-13 21:41:09 +0400

    Initial Commit

---
+++ LilacAppSwiftUI/LilacAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LilacAppSwiftUIApp.swift
+//  LilacAppSwiftUI
+//
+//  Created by Nijat Naghiyev on 13.04.25.
+//
+
+import SwiftUI
+
+@main
+struct LilacAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
